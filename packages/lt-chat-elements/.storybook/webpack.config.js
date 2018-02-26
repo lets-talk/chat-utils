8b8dca1087c095185b1ad3969daffc11765e7b53
sandinosaso <sandinosaso@gmail.com>
2018-02-26 12:57:57 -0300

    Reorder projects folders to be truly monorepo. lt-chat-elements it a project by itself has it own unique dependencies and uses storybook

--- packages/lt-chat-elements/.storybook/webpack.config.js
+++ packages/lt-chat-elements/.storybook/webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, '../packages')
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.(jpg|png|gif|svg)$/,
