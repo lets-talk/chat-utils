4cd345ebf3294925e70eb316034275760e95d624
sandinosaso <sandinosaso@gmail.com>
2018-09-04 13:12:40 -0300

    Added types for implement Grid
    Added types to support Apps, Grid and Positions

--- packages/widgets-manager/src/types.ts
+++ packages/widgets-manager/src/types.ts
@@ -1,3 +1,4 @@
+import { AppPosition } from './types';
 export type PayloadType = 'html' | 'json';
 
 export type ObjectIndex = {
@@ -7,12 +8,105 @@ export type ObjectIndex = {
 export type Settings = {
   css: string;
   inlineCss: ObjectIndex;
+  position: AppPosition;
 }
 
+export type Position = {
+  x: number;
+  y: number;
+}
+
+export enum HTMLFloatType {
+  fixed = "fixed",
+  default = "default"
+};
+
+export type RelativeToElementPosition = {
+  type: 'relative-to-element';
+  payload: {
+    relativeId: string;
+    floatType: HTMLFloatType;
+    offset: {
+      x: number;
+      y: number;
+    },
+  }
+}
+
+export type RelativeToPlacePosition = {
+  type: 'relative-to-position';
+  payload: {
+    positionId: string;
+    offset: {
+      x: number;
+      y: number;
+    },
+  }
+}
+
+export type FixedToTopPosition = {
+  type: 'fixed-to-top-position';
+  payload: {
+    offset: {
+      x: number;
+      y: number;
+    },
+  }
+}
+
+export type AppPosition =
+  RelativeToElementPosition |
+  RelativeToPlacePosition |
+  FixedToTopPosition;
+
 export type App = {
+  id: number;
   name: string;
   payload_type: PayloadType;
   payload: string;
   settings: Settings;
   organization_id: Number;
+}
+
+export type Size = {
+  width: number;
+  height: number;
+}
+
+export type GridCell = {
+  id: string;
+  apps: App[];
+  position: Position;
+  size: Size;
+}
+
+export type GridSettings = {
+  columns: number;
+  gutter: number;
+  padding: number;
+  positions: string[];
+}
+
+export type Grid = {
+  settings: GridSettings;
+  cells: GridCell[];
+}
+
+export interface AddAppsStrategy {
+  add(app: App, apps: App[]): App[];
+}
+
+export interface PositionStrategy {
+  getPositionProps(app: App, cell?: GridCell): ObjectIndex;
+}
+
+export const makeHTMLFloatType = (rawString: string): HTMLFloatType => {
+  switch (rawString) {
+    case HTMLFloatType.fixed:
+    case HTMLFloatType.default:
+      return rawString;
+
+    default:
+      throw Error('Invalid float type');
+  }
 }
