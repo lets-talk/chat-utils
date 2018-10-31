339aaa57937c2958929ce5a9503b60a03374cb98
sandinosaso <sandinosaso@gmail.com>
2018-10-31 01:18:24 -0300

    Change fake Promise for real PostRobot Channel call

--- packages/apps-sdk/src/__tests__/sdk.spec.ts
+++ packages/apps-sdk/src/__tests__/sdk.spec.ts
@@ -1,6 +1,23 @@
 import { SDK } from '../sdk';
 
+import {
+  EVENT_TYPE_EXECUTE_APP_METHOD,
+  EVENT_TYPE_GET_APP_SETTINGS,
+  EVENT_TYPE_LOAD_APP,
+  EVENT_TYPE_REMOVE_APP,
+} from '../constants';
+
+const mockedSend = jest.fn(() => new Promise((resolve) => resolve('OK')));
+const mockChannel = {
+  client: jest.fn(() => ({
+    send: mockedSend,
+  })),
+};
+
 describe('SDK', () => {
+  beforeEach(() => {
+    mockedSend.mockClear();
+  });
   it('Should create a SDK instance without crashing', () => {
     const sdk = new SDK();
     
@@ -8,26 +25,57 @@ describe('SDK', () => {
   });
 
   it('Should call the openApp method', () => {
-    const sdk = new SDK();
+    const sdk = new SDK(() => mockChannel);
+    const expectedSentPayload = { appNamespace: 'letstalk.banner.top-left' };
     
     const result = sdk.openApp('letstalk.banner.top-left');
-    expect(result).toBeInstanceOf(Promise);
+    expect(result).resolves.toBe('lemon');
+    expect(mockedSend).toHaveBeenCalledWith(EVENT_TYPE_LOAD_APP, expectedSentPayload);
   });
   
   it('Should call the closeApp method', () => {
-    const sdk = new SDK();
-    
+    const sdk = new SDK(() => mockChannel);
+    const expectedSentPayload = { appNamespace: 'letstalk.banner.top-left' };
+
     const result = sdk.closeApp('letstalk.banner.top-left');
-    expect(result).toBeInstanceOf(Promise);
+    expect(result).resolves.toBe('lemon');
+    expect(mockedSend).toHaveBeenCalledWith(EVENT_TYPE_REMOVE_APP, expectedSentPayload);
+  });
+
+  it('Should call the getAppSetttings method', () => {
+    const sdk = new SDK(() => mockChannel);
+    const expectedSentPayload = { appNamespace: 'letstalk.banner.top-left' };
+
+    const result = sdk.getAppSettings('letstalk.banner.top-left');
+    expect(result).resolves.toBe('lemon');
+    expect(mockedSend).toHaveBeenCalledWith(EVENT_TYPE_GET_APP_SETTINGS, expectedSentPayload);
   });
   
   it('Should call the openApp method', () => {
-    const sdk = new SDK();
+    const sdk = new SDK(() => mockChannel);
     const params = [
       { vipUser: true },
       true,
-    ]
+    ];
+
+    const expectedSentPayload = {
+      appNamespace: 'letstalk.banner.top-left',
+      payload: {
+        method: 'addChatMetaData',
+        args: params,
+      }
+    }
+
     const result = sdk.executeAppMethod('letstalk.banner.top-left', 'addChatMetaData', params);
-    expect(result).toBeInstanceOf(Promise);
+    expect(result).resolves.toBe('lemon');
+    expect(mockedSend).toHaveBeenCalledWith(EVENT_TYPE_EXECUTE_APP_METHOD, expectedSentPayload);
+  });
+  it('openApp method really resolves to a value', async () => {
+    expect.assertions(1);
+
+    const sdk = new SDK(() => mockChannel);
+    const result = await sdk.openApp('letstalk.banner.top-left');
+    
+    expect(result).toBe('OK');
   });
 });
--- packages/apps-sdk/src/sdk.ts
+++ packages/apps-sdk/src/sdk.ts
@@ -1,25 +1,40 @@
-export class SDK {
+import postRobot from 'post-robot';
 
+import {
+  EVENT_TYPE_GET_APP_SETTINGS,
+  EVENT_TYPE_LOAD_APP,
+  EVENT_TYPE_REMOVE_APP,
+} from './constants';
+
+export class SDK {
+  private channelManager: any;
+  private sendChannel: any;
+
+  constructor(channelFactory = () => postRobot) {
+    super();
+    // Define Communication Channels
+    this.channelManager = channelFactory();
+    this.sendChannel = this.channelManager.client({ window: parent.top, domain: '*' });
+  }
   /**
    * openApp
    * @param appNamespace The app namespace to execute the open
    */
   public openApp(appNamespace: string): Promise<any> {
-    return new Promise((resolve) => resolve({}));
+    return this.sendChannel.send(EVENT_TYPE_LOAD_APP, { appNamespace });
+  }
+  /**
+   * getAppSettings
+   * @param appNamespace The app namespace to execute the open
+   */
+  public getAppSettings(appNamespace: string): Promise<any> {
+    return this.sendChannel.send(EVENT_TYPE_GET_APP_SETTINGS, { appNamespace });
   }
   /**
    * closeApp
    * @param appNamespace The app namespace to execute the close
    */
   public closeApp(appNamespace: string): Promise<any> {
-    return new Promise((resolve) => resolve({}));
-  }
-  /**
-   * ExecuteAppMethod
-   * @param appNamespace The app namespace to execute the executeAppMethod
-   * @param data The data we want to pass to the function as arguments
-   */
-  public executeAppMethod(appNamespace: string, method: string, data: any): Promise<any> {
-    return new Promise((resolve) => resolve(data));
+    return this.sendChannel.send(EVENT_TYPE_REMOVE_APP, { appNamespace });
   }
 }
