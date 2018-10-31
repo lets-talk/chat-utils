276b139976fb764efac69111d2c3d74d9ed836c7
sandinosaso <sandinosaso@gmail.com>
2018-10-31 01:36:02 -0300

    Make SDK class extends from Listener
    Use the addListener and the fire method to disptach actions when we get a message to execute an app method.
    We should see if we have that method registered. If so we it will be fired

--- packages/apps-sdk/src/sdk.ts
+++ packages/apps-sdk/src/sdk.ts
@@ -1,20 +1,32 @@
 import postRobot from 'post-robot';
+import Listener from './listener';
+
+import { EventData } from './types';
 
 import {
+  EVENT_TYPE_EXECUTE_APP_METHOD,
   EVENT_TYPE_GET_APP_SETTINGS,
   EVENT_TYPE_LOAD_APP,
   EVENT_TYPE_REMOVE_APP,
 } from './constants';
 
-export class SDK {
+export class SDK extends Listener {
   private channelManager: any;
   private sendChannel: any;
+  private recieveChannel: any;
 
   constructor(channelFactory = () => postRobot) {
     super();
     // Define Communication Channels
     this.channelManager = channelFactory();
     this.sendChannel = this.channelManager.client({ window: parent.top, domain: '*' });
+    this.recieveChannel = this.channelManager.listener({ window: parent.top, domain: '*' });
+
+    // Register the listeners for recieve messages
+    this.recieveChannel.on(EVENT_TYPE_EXECUTE_APP_METHOD, (event: EventData) => {
+      const { method, args } = event.data;
+      return this.fire(method, args);
+    });
   }
   /**
    * openApp
@@ -36,5 +48,18 @@ export class SDK {
    */
   public closeApp(appNamespace: string): Promise<any> {
     return this.sendChannel.send(EVENT_TYPE_REMOVE_APP, { appNamespace });
+  }
+  /**
+   * ExecuteAppMethod
+   * @param appNamespace The app namespace to execute the executeAppMethod
+   * @param method The method to call
+   * @param args The data we want to pass to the function as arguments
+   */
+  public executeAppMethod(appNamespace: string, method: string, args: any): Promise<any> {
+    return this.sendChannel.send(EVENT_TYPE_EXECUTE_APP_METHOD, { appNamespace, payload: { method, args } });
+  }
+
+  public registerMethod(method: string, callback: () => void) {
+    this.addListener(method, callback);
   }
 }
