b822195f0928eb8fddefe76848641bd6a9e4634d
sandinosaso <sandinosaso@gmail.com>
2018-11-07 22:45:33 -0300

    Add event to ruleStream Update the minor version of this package

--- packages/rule-machine/src/rules.ts
+++ packages/rule-machine/src/rules.ts
@@ -1,7 +1,8 @@
 import { Stream } from "./stream";
 import { LauncherRule, LauncherTrigger } from "./types";
 
-export type CodeEvaluator = (code: string) => CodeResult;
+export type CodeParam = { rule: string, event: any};
+export type CodeEvaluator = (code: CodeParam) => CodeResult;
 export type CodeResult = { type: string };
 
 const makeRuleStream = (
@@ -10,7 +11,12 @@ const makeRuleStream = (
     (rule: LauncherRule) => {
         const allTriggerStreams = rule.triggers.map(setupTrigger)
         const anyTriggerStream = Stream.mergeMap(allTriggerStreams);
-        const ruleStream = anyTriggerStream.map(() => rule.rule);
+        const ruleStream = anyTriggerStream.map((e) => {
+          return {
+            rule: rule.rule,
+            event: e,
+          }
+        });
         return ruleStream;
     }
 
