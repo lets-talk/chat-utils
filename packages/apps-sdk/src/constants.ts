b7e1e962a8885dc14bca250d1578e34b9cca6bc6
sandinosaso <sandinosaso@gmail.com>
2019-07-19 12:11:26 -0400

    When we instantiate the SDK we check for url params
    - We look for 2 new available params: queryParams and initialData
    - If we found queryParams and a mode we use it to define our communication channel (with which window we need to talk to)
    - When popup we need to talk with window.opener otherwise just with window.parent
    - We add tests to check this new behavior
    - Also when obtaining appSettings via the api we extend the result to include initialData passed in the url

--- packages/apps-sdk/src/constants.ts
+++ packages/apps-sdk/src/constants.ts
@@ -3,3 +3,7 @@ export const EVENT_TYPE_LOAD_APP = 'launcher-load-app';
 export const EVENT_TYPE_REMOVE_APP = 'launcher-remove-app';
 export const EVENT_TYPE_GET_APP_SETTINGS = 'launcher-get-app-settings';
 export const EVENT_TYPE_NOTIFY_APP_EVENT = 'launcher-notify-app-event';
+
+// Apps Modes
+export const APP_MODE_IFRAME = 'iframe';
+export const APP_MODE_POPUP = 'popup';
