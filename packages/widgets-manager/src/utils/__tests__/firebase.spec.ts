a54a3f6949930795c7020718934e784cc11ad0a5
sandinosaso <sandinosaso@gmail.com>
2019-10-28 18:34:11 -0300

    Add firebase utils sideEffect
    - This side effect allow us to initialize a Firebase app
    - Save a document, updated a document
    - Execute handler when data changes in the database

---
+++ packages/widgets-manager/src/utils/__tests__/firebase.spec.ts
@@ -0,0 +1,69 @@
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { saveDocument, updateDocument, initializeFirebaseApp } from '../firebase';
+
+// const mockInitializeApp = jest.fn();
+const mockOnAuthStateChanged = jest.fn();
+const mockSignInAnonymously = jest.fn();
+const mockSet = jest.fn();
+
+mockSet.mockReturnValue(true);
+
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(),
+  auth: jest.fn(() => ({
+    onAuthStateChanged: mockOnAuthStateChanged,
+    signInAnonymously: mockSignInAnonymously,
+  })),
+  firestore: () => ({
+    collection: () => ({
+      doc: jest.fn(() => ({
+        set: mockSet
+      }))
+    })
+  }),
+}));
+
+describe('firebase', () => {
+  describe('initializeFirebaseApp', () => {
+    const mockStore = jest.fn();
+
+    initializeFirebaseApp(mockStore);
+
+    it('Call the firebase.auth.onAuthStateChanged', () => {
+      expect(mockOnAuthStateChanged).toBeCalled();
+    });
+    
+    it('Call the firebase.auth.signInAnonymously', () => {
+      expect(mockSignInAnonymously).toBeCalled();
+    });
+  });
+
+  describe('saveDocument', () => {
+    const mockObject = {
+      conversationId: 'MOCK-1',
+      content: '<html>',
+    };
+
+    saveDocument('mock-collection-name', 'myDocId', mockObject);
+
+    it('Call the firebase sdk with proper params', () => {
+      expect(mockSet).toBeCalledWith(mockObject, { merge: true });
+    });
+  });
+  
+  describe('updateDocument', () => {
+    const mockObject = {
+      conversationId: 'MOCK-1',
+      content: '<html>',
+    };
+
+    updateDocument('mock-collection-name', 'myDocId', mockObject);
+
+    it('Call the firebase sdk with proper params', () => {
+      expect(mockSet).toBeCalledWith(mockObject, { merge: true });
+    });
+  });
+})
