9112aa5dfc4438fb9a97209d6d0691edbe1cdab0
sandinosaso <sandinosaso@gmail.com>
2019-10-28 17:39:50 -0300

    Adds mountedApps reducer
    - This reducer is in charge of the mounted_apps part of the state
    - It keeps tracks of whats apps are currently mounted/unmounted

--- packages/widgets-manager/src/reducers/index.ts
+++ packages/widgets-manager/src/reducers/index.ts
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import appsReducer from './apps';
+import mountedAppsReducer from './mounted_apps';
 const rootReducer = combineReducers({
   apps: appsReducer,
+  mounted_apps: mountedAppsReducer,
 })
+
 export {
   rootReducer,
 }
