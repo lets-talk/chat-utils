b8b35e411e96bba8225ed8c8edad5978f03fc185
sandinosaso <sandinosaso@gmail.com>
2019-11-04 14:13:26 -0300

    Having firebase config per environment

--- packages/widgets-manager/src/utils/firebase.ts
+++ packages/widgets-manager/src/utils/firebase.ts
@@ -6,20 +6,33 @@ import { updateUserData, syncData } from '../store/actions';
 
 const debug = require('debug')('widgets-manager:utils:firebase');
 
-const config = {
+const prodConfig = {
   apiKey: "AIzaSyATST9016Y3SCCnyEM0quH1UoCfbv35MEo",
   authDomain: "lt-generic-store.firebaseapp.com",
   databaseURL: "https://lt-generic-store.firebaseio.com",
   projectId: "lt-generic-store",
   storageBucket: "lt-generic-store.appspot.com",
   messagingSenderId: "855806900184",
-  appId: "1:855806900184:web:789d34f4cf40dbd1"
+  appId: "1:855806900184:web:789d34f4cf40dbd1",
 }
+
+const devConfig = {
+  apiKey: "AIzaSyDfbyH9rfq5EQEvNXWRE0x9hX8je309D6A",
+  authDomain: "letstalkwidgetmanagerdemo.firebaseapp.com",
+  databaseURL: "https://letstalkwidgetmanagerdemo.firebaseio.com",
+  projectId: "letstalkwidgetmanagerdemo",
+  storageBucket: "letstalkwidgetmanagerdemo.appspot.com",
+  messagingSenderId: "731912433984",
+  appId: "1:731912433984:web:9144d4095c0b055ecb0aa5",
+}
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 const collectionName = 'users';
 const stateSelector = (state: any) => state;
 
 const initializeFirebaseApp = (store: any): Promise<any> => {
+  debug('Going to instantiate firebase with config:', config);
   Firebase.initializeApp(config);
 
   return new Promise((resolve, reject) => {
