cfc8fdc97df00727a1a77d46459421fd3ffe6170
sandinosaso <sandinosaso@gmail.com>
2019-12-12 16:24:54 -0300

    Update apps selector to retrieve the initialData if present

--- packages/widgets-manager/src/selectors/apps.ts
+++ packages/widgets-manager/src/selectors/apps.ts
@@ -1,4 +1,23 @@
-const selectApps = (state: any) => state.apps;
+import { createSelector } from "reselect"
+import { selectAppsInitialData } from "./initial_data";
+import { App } from "../types";
+
+const debug = require('debug')('widgets-manager:selectors:apps');
+
+const getAllApps = (state: any) => state.apps;
+
+const selectApps = createSelector(
+  getAllApps,
+  selectAppsInitialData,
+  (apps, appsInitialData) => {
+    return apps.map((app: App) => {
+      const appInitialData = appsInitialData[app.slug] || {};
+      debug('selectApps. app, appsInitialData, appInitialData:', app, appsInitialData, appInitialData);
+      return ({ ...app, initialData: appInitialData});
+    });
+  }
+);
+
 
 export {
   selectApps,
