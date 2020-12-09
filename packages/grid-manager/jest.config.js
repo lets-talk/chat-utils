2765e025a703edaa1e155a54b2d16d168a9d8abf
Javiera Vasquez <javiera.vasg@gmail.com>
2020-12-09 19:52:02 +0100

    chore(jest) add minimum coverage to jest and fix the ts interpolation in jest

---
+++ packages/grid-manager/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ["ts", "js"],
+  transform: {"\\.ts$": ['ts-jest']},
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage/",
+  coverageReporters: [
+    "json",
+    "html"
+  ],
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/dist/**"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  testURL: "http://localhost:9000"
+};
